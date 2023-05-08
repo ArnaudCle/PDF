166135f524c8d316491637e9791874f72db6e842
Arnaud Clerentin <Arnaud.Clerentin@u-picardie.fr>
2023-05-08 08:28:04 +0200

    Ajoutez des fichiers projet.

---
+++ PDF/Taille.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF
+{
+    public enum Taille
+    {
+        S,      // 13-18 cm
+        M,      // 15-23 cm
+        L,      // 20-25 cm
+        XL,     // 23-30 cm
+        XXL     // 30-60 cm
+    }
+}
